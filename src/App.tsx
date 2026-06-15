f771590b2cd6902cb5f82c51d055b4204f368cc4
hellstromsamuel <hellstromsamuel@gmail.com>
2026-06-15 09:16:08 +0200

    Initial commit: Vite + React + TS, Tailwind, TanStack Query, Zustand

---
+++ src/App.tsx
@@ -0,0 +1,9 @@
+function App() {
+  return (
+    <div className="grid min-h-svh place-items-center">
+      <h1 className="text-3xl font-bold">Budsjettet</h1>
+    </div>
+  )
+}
+
+export default App
